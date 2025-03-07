16cd9b61d30ceb63413695a98eac61ad50484ca5
taherabbas12 <taherabbasaass@gmail.com>
2025-03-07 22:35:05 +0300

    first commit

--- lib/utils/constants/api_constants.dart
+++ lib/utils/constants/api_constants.dart
@@ -1,16 +1,13 @@
 class ApiConstants {
-  static String baseUrl = 'https://mediaregistration.alkafeel.net/api/';
-  static String baseUrlImage = 'https://taher13.com/api/motrash_2/storage/';
+  static String baseUrl = 'https://aaa.aa.net/api/';
+  static String baseUrlImage = 'https://aa.com/api/a/storage/';
   // Auth User
   static const String login = '/login';
   static const String register = '/register';
   static const String updateProfile = '/customer/update';
   static const String customer = '/customer';
-  static const String section =
-      'https://pic-api.alkafeel.net/v1/api/v1/photos/sections/ar';
-  static const String gallery =
-      'https://pic-api.alkafeel.net/v1/api/v1/photos/photos/ar?page=1';
-//
+
+  //
   //  var url =
   //       'https://pic-api.alkafeel.net/v1/api/v1/photos/photos/ar?page=$page';
   //   if (sectionId != null) {
@@ -23,7 +20,7 @@ class ApiConstants {
   //   }
 
   static String galleryBySection(int sectionId) =>
-      'https://pic-api.alkafeel.net/v1/api/v1/photos/section/$sectionId/ar?page=1';
+      'https://pic-api.aaa.net/v1/api/v1/photos/section/$sectionId/ar?page=1';
   static String videoCast = 'videscats?page=1';
   static String videos(int page, {int? idCast, String? search}) {
     String url = 'videos?page=$page';
